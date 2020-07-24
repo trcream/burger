161faed96581ecc9828cad9a1bb209855fff5a5f
Tcreamer <trentoncreamer@yahoo.com>
2020-07-23 17:59:10 -0700

    updated config file

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 const mysql = require("mysql");
 
+let connection;
+
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else{
-const connection = mysql.createConnection({
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
